77cf575768f45df891fde8b560918864c899fd2a
Selim <selim.tirellil@hotmail.fr>
2023-12-14 10:20:17 +0100

    try to make css work on netlify

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+cp ./node_modules/reveal.js/dist/reset.css ./dist/styles/reset.css
+cp ./node_modules/reveal.js/dist/reveal.css ./dist/styles/reveal.css
+mkdir ./dist/styles/theme
+cp ./node_modules/reveal.js/dist/theme/black.css ./dist/styles/theme/black.css
+mkdir ./dist/styles/highlight
+cp ./node_modules/reveal.js/plugin/highlight/monokai.css ./dist/styles/highlight/monokai.css
